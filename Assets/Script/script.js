a086b859dc5ff49d7cc7819f72b50cbb81942d9a
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-01-27 13:13:27 -0600

    Initial files created
    Includes minute.js - firebase - bootstrap and link to script file in html

---
+++ Assets/Script/script.js
@@ -0,0 +1,11 @@
+//Using Firebase:
+var config = {
+    apiKey: "AIzaSyDwjy8NSU9Lrf25eDNOK7w1ATRuRHcMIbM",
+    authDomain: "train-scheduler-assignme-c1fd7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-assignme-c1fd7.firebaseio.com",
+    projectId: "train-scheduler-assignme-c1fd7",
+    storageBucket: "",
+    messagingSenderId: "931949793399"
+};
+firebase.initializeApp(config);
+
