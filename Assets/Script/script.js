ca6f21a3b410cd3d0543fd8803cce4273000fe86
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-01-27 17:39:25 -0600

    working
    Needs calculations

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -9,3 +9,59 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+$("#submitButton").click(function (event) {
+    // Don't reload the page on click event
+    event.preventDefault();
+
+    // Get form data from HTML
+    var trainName = $("#trainName").val().trim();
+    var trainDestination = $("#trainDestination").val().trim();
+    var firstTrainTime = $("#trainTime").val().trim();
+    var trainFreq = parseInt($("#trainFreq").val().trim());
+
+    logIt();
+    function logIt() {
+        console.log("------------------------------------------")
+        console.log("Train Name: " + trainName);
+        console.log("Train Destination: " + trainDestination);
+        console.log("First Train Time: " + firstTrainTime);
+        console.log("Train Frequency: " + trainFreq);
+        console.log("------------------------------------------")
+    }
+
+    // Put the details of the new train in Firebase
+    database.ref().push({
+        trainNameDB: trainName,
+        trainDestinationDB: trainDestination,
+        firstTrainTimeDB: firstTrainTime,
+        trainFreqDB: trainFreq,
+        addedDB: moment().format("LT"),
+    })
+});
+
+database.ref().on("child_added", function (trainDBinfo) {
+
+    console.log(trainDBinfo.val().firstTrainTimeDB);
+    console.log(trainDBinfo.val().trainFreqDB + " minutes");
+
+    $("tbody").append(
+        '<tr>' +
+        '<td>' + '<img src="assets/images/train.png"> ' + trainDBinfo.val().trainNameDB + '</td>' +
+        '<td>' + trainDBinfo.val().trainDestinationDB + '</td>' +
+        '<td>' + trainDBinfo.val().trainFreqDB + " min(s)" + '</td>' +
+        // '<td>' + nextArrival + '</td>' +
+        '</tr>'
+    )
+});
+
+
+$(document).ready(function () {
+    $("#timeNow").text(moment().format("LTS"));
+    $("#timeUTC").text(moment.utc().format("LTS"));
+    setInterval(function () {
+        $("#timeNow").text(moment().format("LTS"));
+        $("#timeUTC").text(moment.utc().format("LTS"));
+    }, 1000);
+});
